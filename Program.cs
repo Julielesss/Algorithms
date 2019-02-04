9b085c5094100cc2eaf125c9188168255c8db337
Julielesss <36435376+Julielesss@users.noreply.github.com>
2019-02-04 23:00:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FactorialRecursive(5));
+            Console.WriteLine(FactorialIterative(5));
+
+        }
+
+
+        static long FactorialRecursive(uint number)
+        {
+            if (number == 2)
+                return 2;
+            if (number < 2)
+                return 1;
+
+            return number * FactorialRecursive(--number);
+        }
+
+
+        static long FactorialIterative(uint number)
+        {
+            if (number < 2)
+                return 1;
+            if (number == 2)
+                return 2;
+
+            long result = number--;
+            while (number > 1)
+                result *= number--;
+
+            return result;
+        }
+    }
+}
